8fa91bf787f17544f486c1afb0318a6f20da1717
David Eastmond <davebuck1981@gmail.com>
2022-10-30 06:55:00 -0400

    update patch controller for sources

---
+++ src/controllers/user/patch/patch.source.controller.ts
@@ -0,0 +1,23 @@
+import {
+  ISecureUser,
+  IUserDocument,
+  TCustomSourceFilter,
+} from "../../../models/user/user.schema.types";
+import { UserUtils } from "../utils/user-utils";
+
+export async function patchNewsSource(
+  this: IUserDocument,
+  sourceData: {
+    list: { id: string; name: string }[];
+    option: TCustomSourceFilter;
+  }
+): Promise<ISecureUser> {
+  if (sourceData.option === "none") {
+    this.configuration.sources.list = [];
+  } else {
+    this.configuration.sources.list = sourceData.list;
+  }
+  this.configuration.sources.option = sourceData.option;
+  const refreshedUser = await this.save();
+  return UserUtils.toSecureUser(refreshedUser);
+}
