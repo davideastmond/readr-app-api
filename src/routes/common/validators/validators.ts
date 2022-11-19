5c78a714d06d4e5323a15cae7a63d2603e9e4c0b
David Eastmond <davebuck1981@gmail.com>
2022-11-19 08:55:40 -0500

    pageSize query param

--- src/routes/common/validators/validators.ts
+++ src/routes/common/validators/validators.ts
@@ -1,13 +1,13 @@
-import { body } from "express-validator";
+import { query } from "express-validator";
 const MAX_PAGE_SIZE = 250;
 
 // Validate the pageSize parameter (optional)
 export const getPageSizeValidator = (): any[] => {
   return [
-    body("pageSize").optional(),
-    body("pageSize").custom((value) => {
+    query("pageSize").optional(),
+    query("pageSize").custom((value) => {
       if (!value) return true;
-      if (!Number.isInteger(value)) return false;
+      if (!Number.isInteger(parseInt(value))) return false;
       if (value > MAX_PAGE_SIZE) return false;
       return true;
     }),
