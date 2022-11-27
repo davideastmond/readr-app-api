8c5d6b3f70433f39d3deb8f74eefaff1ba949656
David Eastmond <davebuck1981@gmail.com>
2022-11-27 17:32:32 -0500

    use pageSize from user document

--- src/routes/user/controllers/user.controller.get.ts
+++ src/routes/user/controllers/user.controller.get.ts
@@ -5,12 +5,13 @@ import { IUserDocument } from "../../../models/user/user.schema.types";
 export const getFeed = async (req: Request, res: Response) => {
   try {
     const { user }: { user: IUserDocument } = res.locals as any;
-    const { pageSize } = req.body;
     const topics = user.configuration.topics;
+
+    const { headlines: pageSize } = user.configuration.pageSize;
 
     if (!topics || topics.length === 0) {
       const newsClient = new NewsClient();
-      const headlines = await newsClient.fetchHeadlines(pageSize || 35);
+      const headlines = await newsClient.fetchHeadlines(pageSize);
       return res.status(200).send(headlines);
     } else {
       const newsClient = new NewsClient();
@@ -24,6 +25,28 @@ export const getFeed = async (req: Request, res: Response) => {
           msg: `${err.message}: Server error - cannot get feed`,
           param: "n/a",
           location: "get feed",
+        },
+      ],
+    });
+  }
+};
+
+export const getUserHeadlines = async (req: Request, res: Response) => {
+  // Get the authenticated user's version of headlines so we can use their settings
+  // to control page size
+  try {
+    const { user }: { user: IUserDocument } = res.locals as any;
+    const { headlines: pageSize } = user.configuration.pageSize;
+    const newsClient: NewsClient = new NewsClient();
+    const headlines = await newsClient.fetchHeadlines(pageSize);
+    return res.status(200).send(headlines);
+  } catch (err) {
+    return res.status(500).send({
+      errors: [
+        {
+          msg: `${err.message}: Server error - cannot get user headlines`,
+          param: "n/a",
+          location: "get user headlines",
         },
       ],
     });
